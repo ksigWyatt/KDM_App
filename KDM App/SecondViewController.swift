f3e9494aec0a90a38c3b37a4a28dbae4a2ef0ff4
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-06-30 21:43:49 -0400

    Minor UI Changes - PT2

--- KDM App/SecondViewController.swift
+++ KDM App/SecondViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var addButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        addButton.layer.cornerRadius = 0.5 * addButton.bounds.size.width
+        addButton.layer.borderColor = UIColor(red:0.0/255.0, green:122.0/255.0,
+                                              blue:255.0/255.0, alpha:1).cgColor as CGColor
+        addButton.layer.borderWidth = 2.0
+        addButton.clipsToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
