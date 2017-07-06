a932d319a42376ad7cb82115552a9a0c9e622386
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-07-06 00:31:56 -0400

    Major UI Changes

---
+++ KDM App/Dronez.swift
@@ -0,0 +1,22 @@
+//
+//  Dronez.swift
+//  KDM App
+//
+//  Created by Wyatt Baggett on 7/5/17.
+//  Copyright © 2017 Kingdom Digital Media. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Dronez: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
